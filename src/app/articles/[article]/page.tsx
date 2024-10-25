6accee81f2788e417fd65af0b83188d5b1ca272d
Nasser Setti <abdennasseressati@gmail.com>
2024-10-25 15:24:40 +0100

    chore: initial commit

---
+++ src/app/articles/[article]/page.tsx
@@ -0,0 +1,35 @@
+type Article = {
+  title: string;
+  content: string;
+};
+
+type PageProps = {
+  params: Record<string, string>;
+};
+
+const mockArticles = async () => {
+  return new Promise<Article[]>((resolve) => {
+    resolve([]);
+  });
+};
+
+const mockSingleArticle = async (slug: string) => {
+  return new Promise<Article>((resolve) => {
+    resolve({ title: slug, content: "Hello World" });
+  });
+};
+
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const posts = await mockArticles();
+
+  return posts.map((post) => ({
+    slug: post.title,
+  }));
+}
+
+export default async function Page({ params: { article } }: PageProps) {
+  const articleData = await mockSingleArticle(article);
+  return <h1>{articleData.content}</h1>;
+}
